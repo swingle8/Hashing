55394d377a9a07185789bfc80ec5e2869da8c96d
Shivam Singhal <swingle8@gmail.com>
2020-07-19 17:21:37 +0530

    Update MaxWindowSubString.java

--- MaxWindowSubString.java
+++ MaxWindowSubString.java
@@ -1,14 +1,14 @@
 //https://leetcode.com/problems/minimum-window-substring/
-Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+// Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
 
-Example:
+// Example:
 
-Input: S = "ADOBECODEBANC", T = "ABC"
-Output: "BANC"
-Note:
+// Input: S = "ADOBECODEBANC", T = "ABC"
+// Output: "BANC"
+// Note:
 
-If there is no such window in S that covers all characters in T, return the empty string "".
-If there is such window, you are guaranteed that there will always be only one unique minimum window in S.
+// If there is no such window in S that covers all characters in T, return the empty string "".
+// If there is such window, you are guaranteed that there will always be only one unique minimum window in S.
 
 
 
