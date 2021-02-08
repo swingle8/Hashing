446a71df23931f0c7e087e815fef9c708d31509b
Shivam Singhal <swingle8@gmail.com>
2021-02-08 17:58:18 +0530

    Add files via upload

---
+++ Largest subarray with 0 sum.java
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+import java.util.*;
+
+class MaxLenZeroSumSub
+{
+
+    // Returns length of the maximum length subarray with 0 sum
+
+    // Drive method
+    public static void main(String arg[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0)
+        {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = sc.nextInt();
+
+            GfG g = new GfG();
+            System.out.println(g.maxLen(arr, n));
+            T--;
+        }
+    }
+}// } Driver Code Ends
+
+
+
+class GfG
+{
+    int maxLen(int arr[], int n)
+    {
+        int ans =  0;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        map.put(0, -1);
+        int sum = 0;
+        for (int i = 0 ; i < n ; i++) {
+            sum = sum + arr[i];
+            if (map.containsKey(sum)) {
+                int distance = i - map.get(sum);
+                ans = Math.max(ans, distance);
+            }
+            else {
+                map.put(sum, i);
+            }
+        }
+        return ans;
+    }
+}
